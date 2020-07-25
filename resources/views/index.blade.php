b511582039cb5b0b00d824419aa4ddd59afda18e
HaniFatimah <hanifatimah008@gmail.com>
2020-07-25 09:57:26 +0700

    commit tugas 2 laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>My Profile</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#profil">Profile <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#experience">Experience</a>
+                <a class="nav-item nav-link" href="#portofolio">Protofolio</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+        <img src="gambar/human.png" width="150px" class="rounded-circle">
+            <br>
+          <h2 class="display-6">Hani Fatimah</h2>
+            <a class="lead">
+                Student at SMK Taruna Bhakti Depok
+            </a>
+            <hr>
+            <a>
+                <p><img src="gambar/email.png" width="30px" class="rounded-circle"> hanifatimah008@gmail.com </p>
+                <p><img src="gambar/ig.png" width="30px" class="rounded-circle"> hnifaah_x</p>
+            </a>
+            
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h2 id="profil">My Profile</h2><br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-center">
+               Nama saya Hani Fatimah. Saya saat ini adalah pelajar di sekolah SMK Taruna Bhakti, saya sekarang duduk dikelas XII dan Jurusan RPL (Rekayasa Perangkat Lunak). 
+               Saya berumu 17 Tahun, saya memiliki 2 orang kakak. Saya lahir pada tanggal 08 Agustus 2002 di Kota Depok, sata beragama Islam .Cita-cita  saya adalah ingin menjadi seorang Proggramer yang Hebat. 
+               Berikut Biodata saya dibawah ini :
+               <br><br>
+               Nama           : Hani Fatimah
+               <br>
+               Kelas           : XII RPL 1
+               <br>
+               Sekolah         : SMK Taruna Bhakti
+               <br>
+               Tanggal Lahir   : 08 Agustus 2002
+               <br>
+               Tempat Lahir    : Depok
+               <br>
+               Agama           : Islam
+               <br>
+               Alamat          : Gas Alam, Kampung Baru Rt.02/Rw.07
+
+               
+               <br><br><br>
+            </div>
+        </div>
+    </div>
+
+    <hr>    
+
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h2 id="experience">Experience</h2><br>
+            </div>
+         </div>
+
+        <div class="row">
+        <div class="col text-center">
+            Nama Saya Hani Fatimah Dari sekolah SMK TARUNA BHAKTI DEPOK. Pengalaman saya selama sekolah saya sudah banyak mempelajari banyak ilmu dari
+            guru-guru disana. Selama belajar Pemrograman yang itu juga adalah Jurusan saya, saya sudah mempelajari bagaimana membuat aplikasi penjualan sederhana,
+            mempelajari cara membuat website sederhana dan lainnya. Dan saat ini saya sedang belajar Pemrograman Laravel. <br>
+            Dan ini juga beberapa pengelaman saya yang lain :
+
+            <br><br>
+
+<div class="row row-cols-1 row-cols-md-3">
+  <div class="col mb-4">
+    <div class="card h-100">
+      <img src="gambar/kunjin.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Kunjungan Industri</h5>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card h-100">
+      <img src="gambar/tutor.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Tutor Sebaya TEI</h5>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card h-100">
+      <img src="gambar/rpl.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Tutor Sebaya RPL</h5>
+      </div>
+    </div>
+  </div>
+  
+</div>
+            
+            <br><br>
+        </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col text-center">
+                <h2 id="portofolio">Portofolio</h2><br>
+                <div class="card-deck">
+
+            <div class="card">
+                <img src="gambar/ser.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                 <br><br><br><br>
+                 <h5 class="card-title">Mengikuti Kelas Dicoding Pemrograman Web Pemula</h5>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="gambar/web.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                 <h5 class="card-title">Membuat Website Sederhana</h5>
+                 <p class="card-text"></p>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="gambar/pen.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                <br><br>
+                 <h5 class="card-title">Membuat Aplikasi Transaksi Berbasis Dekstop</h5>
+                 <p class="card-text"></p>
+                </div>
+            </div>
+ 
+        </div>
+
+    </div>
+                
+            </div>
+        </div>
+     </div>
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+</body>
+</html>
